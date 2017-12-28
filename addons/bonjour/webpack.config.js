908cd61068911a408607a8d443a83a8ebb623b5d
shellscape <andrew@shellscape.org>
2017-12-27 23:14:43 -0500

    add 'on' feature and bonjour addon

---
+++ addons/bonjour/webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+
+function broadcast(port) {
+  const bonjour = require('bonjour')(); // eslint-disable-line
+  bonjour.publish({
+    name: 'Some Bitchin\' App',
+    port,
+    type: 'http',
+    subtypes: ['webpack']
+  });
+  process.on('exit', () => {
+    bonjour.unpublishAll(() => {
+      bonjour.destroy();
+    });
+  });
+}
+
+module.exports = {
+  entry: {
+    index: [path.resolve(__dirname, 'app.js')]
+  },
+  mode: 'development',
+  output: {
+    filename: 'output.js'
+  }
+};
+
+module.exports.serve = {
+  content: [__dirname],
+  on: {
+    listen(server) {
+      broadcast(server.address().port);
+    }
+  }
+};
