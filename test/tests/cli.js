17f76528de67c2b7bc51a89875900cf9431b2854
shellscape <andrew@shellscape.org>
2018-01-29 22:27:42 -0500

    adding cli tests

--- test/tests/cli.js
+++ test/tests/cli.js
@@ -1,8 +1,54 @@
 'use strict';
 
+const path = require('path');
+const assert = require('power-assert');
+const execa = require('execa');
+const fetch = require('node-fetch');
+
+const cliPath = path.resolve(__dirname, '../../cli.js');
+const configPath = path.resolve(__dirname, '../fixtures/basic/webpack.config-silent.js');
+
 describe('webpack-serve CLI', () => {
-  it('should run webpack-serve [config]');
-  it('should run webpack-serve --config');
+  it('should run webpack-serve [config]', (done) => {
+    const proc = execa(cliPath, [configPath]);
+
+    setTimeout(() => {
+      fetch('http://localhost:8080')
+        .then((res) => {
+          assert(res.ok);
+          proc.kill('SIGINT');
+          done();
+        });
+    }, 500);
+  });
+
+  it('should run webpack-serve --config', (done) => {
+    const proc = execa(cliPath, ['--config', configPath]);
+
+    setTimeout(() => {
+      fetch('http://localhost:8080')
+        .then((res) => {
+          assert(res.ok);
+          proc.kill('SIGINT');
+          done();
+        });
+    }, 500);
+  });
+
+  it('should run webpack-serve and find the config', (done) => {
+    const proc = execa(cliPath, { cwd: path.resolve(__dirname, '../fixtures/basic') });
+
+    setTimeout(() => {
+      fetch('http://localhost:8080')
+        .then((res) => {
+          assert(res.ok);
+          proc.kill('SIGINT');
+          done();
+        });
+    }, 500);
+  });
+
+
   it('should use the --content flag');
   it('should use the --dev flag');
   it('should use the --host flag');
--- test/tests/options.js
+++ test/tests/options.js
@@ -1,6 +1,12 @@
 'use strict';
 
+// const assert = require('power-assert');
+// const fetch = require('node-fetch');
+// const serve = require('../../');
+// const { load } = require('../util');
+
 describe('webpack-serve Options', () => {
+  it('should accept an add option');
   it('should accept a compiler option');
   it('should accept a config option');
   it('should accept a content option');
@@ -11,6 +17,6 @@ describe('webpack-serve Options', () => {
   it('should accept a https option');
   it('should accept a logLevel option');
   it('should accept a logTime option');
-  it('should accept a open option');
+  it('should accept an open option');
   it('should accept a port option');
 });
