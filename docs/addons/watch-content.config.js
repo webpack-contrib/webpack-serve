d2ba92ca81cc213c984ec633e059db6b888000f4
Andrew Powell <shellscape@users.noreply.github.com>
2018-03-26 12:10:36 -0700

    add watch-content addon example (#76)

---
+++ docs/addons/watch-content.config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const path = require('path');
+const chokidar = require('chokidar');
+const stringify = require('json-stringify-safe');
+const { WebSocket } = require('ws');
+
+
+module.exports = {
+  entry: {
+    index: [path.resolve(__dirname, 'app.js')]
+  },
+  mode: 'development',
+  output: {
+    filename: 'output.js'
+  }
+};
+
+module.exports.serve = {
+  content: [__dirname],
+  hot: {
+    host: 'localhost',
+    port: 8090
+  },
+  on: {
+    listen(server) {
+      const socket = new WebSocket('ws://localhost:8090');
+      const watchPath = __dirname;
+      const options = {};
+      const watcher = chokidar.watch(watchPath, options);
+
+      watcher.on('change', () => {
+        const data = {
+          type: 'broadcast',
+          data: {
+            type: 'window-reload',
+            data: {}
+          }
+        };
+
+        socket.send(stringify(data));
+      });
+
+      server.on('close', () => {
+        watcher.close();
+      });
+    }
+  }
+};
